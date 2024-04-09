71905a9d5a590cb311f6ed6745a15c05fbc72a47
RohaanUmair <m.rohaanumair@gmail.com>
2024-04-09 20:29:09 +0500

    day39 added

---
+++ day39/q115/main.ts
@@ -0,0 +1,32 @@
+function logDayOfWeek(dayNumber: number): void {
+    switch (dayNumber){
+        case 1:
+            console.log('Sunday');
+            break;
+        case 2:
+            console.log('Monday');
+            break;
+        case 3:
+            console.log('Tuesday');
+            break;
+        case 4:
+            console.log('Wednesday');
+            break;
+        case 5:
+            console.log('Thursday');
+            break;
+        case 6:
+            console.log('Friday');
+            break;
+        case 7:
+            console.log('Saturday');
+            break;
+        default:
+            console.log('Invalid day number');
+            break;
+    }
+}
+
+
+logDayOfWeek(3);   // Outputs 'Tuesday'
+logDayOfWeek(8);   // Outputs 'Invalid day number'
